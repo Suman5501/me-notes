58bf740bd9f1539879fe8faaa700f39c5afb21d2
Suman Kumari <sumankri5501@gmail.com>
2020-10-31 02:12:42 +0530

    TaskActivity updated

---
+++ app/src/main/java/com/example/mail_me/TaskActivity.java
@@ -0,0 +1,14 @@
+package com.example.mail_me;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TaskActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.content_task);
+    }
+}
